2680f2f95f2091d278c973d82309aa96cad56e8c
侯伟轩 <wxhou@yunjinginc.com>
2020-06-08 18:38:24 +0800

    update base

--- basic/base.py
+++ basic/base.py
@@ -62,27 +62,29 @@ class AirtestPoco(object):
         temp = air_api.Template(r"%s" % img_name, rgb=rgb, record_pos=record_pos, resolution=d.screen)
         return temp
 
-    def touch(self, v: str, *args, **kwargs):
-        """触摸函数"""
-        air_api.touch(self.template(v), *args, **kwargs)
+    def touch(self, v, **kwargs):
+        """
+        在设备屏幕上执行触摸操作
+        :param v: 要触摸的目标，可以是Template实例，也可以是绝对坐标（x，y）
+        :param kwargs: [times  要执行多少次触摸]
+        """
+        air_api.touch(self.template(v), **kwargs)
 
     def double_click(self, v: str):
         """双击"""
         air_api.double_click(self.template(v))
 
-    def swipe(self, v1: str, v2=None, vector=None, **kwargs):
+    def swipe(self, v1, v2=None, vector=None, **kwargs):
         """
         在设备屏幕上执行滑动操作。
         分配参数有两种方法
             swipe(v1, v2=Template(...)) ＃从v1滑动到v2
             swipe(v1, vector=(x, y)) ＃滑动从v1开始并沿向量移动。
-        参数：
-            v1 –滑动的起点，可以是Template实例，也可以是绝对坐标（x，y）
-            v2 –滑动的终点，可以是Template实例，也可以是绝对坐标（x，y）
-            向量 –滑动动作的向量坐标，可以是绝对坐标（x，y）或屏幕百分比，例如（0.5，0.5）
-            **矮人 –
-            平台特定的kwargs，请参考相应的文档
-        引发：	异常 –如果提供的参数不足以执行交换操作，则为一般异常
+        :param：v1 –滑动的起点，可以是Template实例，也可以是绝对坐标（x，y）
+        :param：v2 –滑动的终点，可以是Template实例，也可以是绝对坐标（x，y）
+        :param：vector - 向量 –滑动动作的向量坐标，可以是绝对坐标（x，y）或屏幕百分比，例如（0.5，0.5）
+        :param：**kwargs – 平台特定的kwargs，请参考相应的文档
+        :exception: 异常 –如果提供的参数不足以执行交换操作，则为一般异常
         平台：	Android，Windows，iOS
         :return: 原点位置和目标位置
         """
@@ -92,23 +94,41 @@ class AirtestPoco(object):
             v2 = self.template(v2)
         return air_api.swipe(v1, v2, vector, **kwargs)
 
-    def wait(self, v, *args, **kwargs):
-        """等待函数"""
-        air_api.wait(self.template(v), *args, **kwargs)
+    def wait(self, v: str, **kwargs):
+        """
+        等待与设备屏幕上的模板匹配
+        :param v –等待的目标对象，模板实例
+        :param 超时 –等待比赛的时间间隔，默认为无，即ST.FIND_TIMEOUT
+        :param interval –尝试找到匹配项的时间间隔（以秒为单位）
+        :param intervalfunc –在每次未成功尝试找到相应匹配项后调用
+        """
+        air_api.wait(self.template(v), **kwargs)
 
-    def exists(self, v):
-        """判断函数"""
+    def exists(self, v: str):
+        """
+        检查设备屏幕上是否存在给定目标
+        :param v –检查对象
+        :return 如果找不到目标，则为False，否则返回目标的坐标
+        """
         return air_api.exists(self.template(v))
 
-    def assert_exists(self, v, msg: str = None):
-        """断言函数"""
+    def assert_exists(self, v: str, msg: str = None):
+        """
+        断言目标存在于设备屏幕上
+        :param v –要检查的目标
+        :param msg –断言的简短描述，它将记录在报告中
+        """
         air_api.assert_exists(self.template(v), msg)
 
-    def assert_not_exists(self, v, msg: str = None):
-        """断言函数"""
+    def assert_not_exists(self, v: str, msg: str = None):
+        """
+        断言目标在设备屏幕上不存在
+        :param v –要检查的目标
+        :param msg –断言的简短描述，它将记录在报告中
+        """
         air_api.assert_not_exists(self.template(v), msg)
 
-    def find_all(self, v):
+    def find_all(self, v: str):
         """
         在设备屏幕上查找目标的所有位置并返回其坐标
         :param v:要查找的目标
@@ -123,55 +143,55 @@ class AirtestPoco(object):
 
     def poco_wait_any(self, objects):
         """
-        等待，直到所有给定的 UI 代理在超时之前显示。将定期轮询所有 UI 代理。
+        等待，直到所有给定的一个 UI 代理在超时之前显示。将定期轮询所有 UI 代理。
         :param objects:
         :return: bool
         """
         try:
-            return self.poco.wait_for_any(objects, timeout=air_api.ST.FIND_TIMEOUT)
+            return self.poco.wait_for_any(objects, timeout=self.timeout)
         except poco_error.PocoTargetTimeout:
             return False
 
     def poco_wait_all(self, objects):
         """
-        等待，直到所有给定的 UI 代理在超时之前显示。将定期轮询所有 UI 代理。
+        等待，直到所有给定的所有 UI 代理在超时之前显示。将定期轮询所有 UI 代理。
         :param objects:
         :return:
         """
         try:
-            self.poco.wait_for_all(objects, timeout=air_api.ST.FIND_TIMEOUT)
+            self.poco.wait_for_all(objects, timeout=self.timeout)
             return True
         except poco_error.PocoTargetTimeout:
             return False
 
-    def poco_obj(self, *args, **kwargs):
+    def poco_obj(self, **kwargs):
         """poco实例"""
         if 'index' in kwargs:
             index = kwargs.pop('index')
-            ele = self.poco(*args, **kwargs)[index]
+            ele = self.poco(**kwargs)[index]
         else:
-            ele = self.poco(*args, **kwargs)
+            ele = self.poco(**kwargs)
         ele.wait_for_appearance(timeout=self.timeout)
         return ele
 
-    def poco_click(self, *args, **kwargs):
+    def poco_click(self, **kwargs):
         """
         对由UI代理表示的UI元素执行click操作。如果这个UI代理代表一组
         UI元素，单击集合中的第一个元素，并将UI元素的定位点用作默认值
         一个。还可以通过提供“focus”参数单击另一个点偏移。
-        :param args:
         :param kwargs: [text, name]
         """
         log("点击元素：{}".format(kwargs))
-        self.poco_obj(*args, **kwargs).click()
+        self.poco_obj(**kwargs).click()
         self.poco.wait_stable()
 
     def poco_click_pos(self, pos):
         """
         在给定坐标下对目标设备执行单击(触摸，轻击等)操作。坐标(x, y)是一个2-列表或2-元组。
         x和y的坐标值必须在0 ~ 1之间，以表示屏幕的百分比。
-        例如，坐标[0.5,0.5]表示屏幕的中心，坐标[0,0]表示左上角。
-        有关坐标系统的详细信息，请参阅CoordinateSystem。
+        例如：
+            坐标[0.5,0.5]表示屏幕的中心，坐标[0,0]表示左上角。
+            有关坐标系统的详细信息，请参阅CoordinateSystem。
         实际案例：
             单击分辨率为（1920，1080）的屏幕的（100，100）点：
                 poco.click([100.0 / 1920, 100.0 / 1080])
@@ -180,35 +200,34 @@ class AirtestPoco(object):
         """
         self.poco.click(pos)
 
-    def poco_get_text(self, *args, **kwargs):
+    def poco_text(self, **kwargs):
         """
         获取 UI 元素的文本属性。如果没有此类属性，则返回"无"。
-        :param args:
         :param kwargs:
-        :return:
+        :return: txt
         """
-        txt = self.poco_obj(*args, **kwargs).get_text()
+        txt = self.poco_obj(**kwargs).get_text()
         log("获取元素{}文本：{}".format(kwargs, txt))
         return txt
 
-    def poco_attr(self, name, *args, **kwargs):
+    def poco_attr(self, name, **kwargs):
         """
         按给定属性名称检索 UI 元素的属性。如果属性不存在，则返回"无"。
-        visible：用户是否可见
-        text：UI 元素的字符串值
-        type：远程运行时的 UI 元素的类型名称
-        pos：UI 元素的位置
-        size：根据屏幕，0+1 范围内的百分比大小 [宽度、高度]
-        name：UI 元素的名称
-        ...： other sdk 实现的属性
+            visible：用户是否可见
+            text：UI 元素的字符串值
+            type：远程运行时的 UI 元素的类型名称
+            pos：UI 元素的位置
+            size：根据屏幕，0+1 范围内的百分比大小 [宽度、高度]
+            name：UI 元素的名称
+            ...： other sdk 实现的属性
         :return:
         """
-        self.poco_obj(*args, **kwargs).attr(name)
+        self.poco_obj(**kwargs).attr(name)
 
-    def poco_freeze(self, *args, **kwargs):
+    def poco_freeze(self, **kwargs):
         """冻结UI树并返回当前的UI结果树"""
         with self.poco.freeze() as freeze:
-            dump = freeze(*args, **kwargs)
+            dump = freeze(**kwargs)
         return dump
 
     def poco_hierarchy_dict(self):
@@ -217,13 +236,12 @@ class AirtestPoco(object):
         hierarchy_dict = frozen_poco.agent.hierarchy.dump()
         return hierarchy_dict
 
-    def poco_exists(self, *args, **kwargs):
+    def poco_exists(self, **kwargs):
         """
         测试UI元素是否在层次结构中
-        :param args:
         :param kwargs: [text,name]
         """
-        result = self.poco_freeze(*args, **kwargs).exists()
+        result = self.poco_freeze(**kwargs).exists()
         log("元素{}验证结果: {}".format(kwargs, result))
         return result
 
